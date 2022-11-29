b38c68b97b77d1774594769871733403bf02dbeb
At1sts <petals89@naver.com>
2022-11-29 22:22:00 +0900

    js

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,2 +1,15 @@
 'use strict';
 
+let scrollA = 0;
+const nav=document.querySelector('nav');
+
+window.addEventListener('scroll',()=>{
+  if(scrollA < window.scrollY) {
+        nav.classList.add('nav--up');
+  }else {
+    nav.classList.remove('nav--up');
+  }
+  scrollA = window.scrollY;
+})
+
+
