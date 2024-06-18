0b491badbec2ffd6952b17bbbff74080c8285a4d
At1st <petals89@naver.com>
2024-06-18 12:12:53 +0900

    Update main.js

--- assets/src/main.js
+++ assets/src/main.js
@@ -11,3 +11,16 @@ window.addEventListener('scroll',()=>{
   }
   scrollA = window.scrollY;
 });
+
+
+
+let images = document.querySelectorAll('#slideshow img');
+let currentIndex = 0;
+
+function showNextImage() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add('active');
+}
+
+setInterval(showNextImage, 3000); // 3초마다 이미지 전환
