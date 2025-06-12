0e75aed141f7d8ba4bd283b58bd7579f8ed6c479
At1st <petals89@naver.com>
2025-06-13 01:30:59 +0900

    z

--- assets/src/1_book_circle.js
+++ assets/src/1_book_circle.js
@@ -63,7 +63,7 @@ book_list_wrap.addEventListener("mouseleave", () => {
 
 function updateRadius() {
   const vw = window.innerWidth;
-  radius = Math.min(vw * 0.3, 800); // 반지름 다시 계산
+  radius = Math.min(vw * 0.6, 800); // 반지름 다시 계산
   
   const boxes = document.querySelectorAll(".book_item");
   boxes.forEach((box, i) => {
