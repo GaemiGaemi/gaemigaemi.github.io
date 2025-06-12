08db9e9adcd8b489d6541ab5bc17cd6ba3bec9eb
At1st <petals89@naver.com>
2025-06-12 23:00:05 +0900

    zz

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,31 +1,31 @@
 'use strict';
 
 let scrollA = 0;
-const nav=document.querySelector('nav');
+const nav = document.querySelector('nav');
 
-window.addEventListener('scroll',()=>{
-  if(scrollA < window.scrollY) {
+window.addEventListener('scroll', () => {
+  if (scrollA < window.scrollY) {
     nav.classList.add('nav--up');
-  }else {
+  } else {
     nav.classList.remove('nav--up');
   }
   scrollA = window.scrollY;
 });
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const slides = document.querySelectorAll('#slideshow img');
   let currentIndex = 0;
 
   function showSlide(index) {
-      slides.forEach((slide, i) => {
-          slide.classList.toggle('active', i === index);
-      });
+    slides.forEach((slide, i) => {
+      slide.classList.toggle('active', i === index);
+    });
   }
 
   function nextSlide() {
-      currentIndex = (currentIndex + 1) % slides.length;
-      showSlide(currentIndex);
+    currentIndex = (currentIndex + 1) % slides.length;
+    showSlide(currentIndex);
   }
 
   showSlide(currentIndex);
@@ -35,24 +35,24 @@ document.addEventListener('DOMContentLoaded', function() {
 
 
 
-    function updateDateTime() {
-      const now = new Date();
-      
-      const year = now.getFullYear();
-      const month = String(now.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작
-      const day = String(now.getDate()).padStart(2, '0');
-      const hours = String(now.getHours()).padStart(2, '0');
-      const minutes = String(now.getMinutes()).padStart(2, '0');
-      const seconds = String(now.getSeconds()).padStart(2, '0');
+function updateDateTime() {
+  const now = new Date();
 
-      const dateStr = `${year}-${month}-${day}`;
-      const timeStr = `${hours}:${minutes}:${seconds}`;
-      
-      document.getElementById('datetime').textContent = `${dateStr} ${timeStr}`;
-    }
+  const year = now.getFullYear();
+  const month = String(now.getMonth() + 1).padStart(2, '0'); // 월은 0부터 시작
+  const day = String(now.getDate()).padStart(2, '0');
+  const hours = String(now.getHours()).padStart(2, '0');
+  const minutes = String(now.getMinutes()).padStart(2, '0');
+  const seconds = String(now.getSeconds()).padStart(2, '0');
 
-    setInterval(updateDateTime, 1000); // 1초마다 갱신
-    updateDateTime(); // 초기 실행
+  const dateStr = `${year}-${month}-${day}`;
+  const timeStr = `${hours}:${minutes}:${seconds}`;
+
+  document.getElementById('datetime').textContent = `${dateStr} ${timeStr}`;
+}
+
+setInterval(updateDateTime, 1000); // 1초마다 갱신
+updateDateTime(); // 초기 실행
 
 
 
